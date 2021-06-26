bc482968e910398c907c6ea6585e033af5c48d21
Sandorik00 <sasha.kobec00@gmail.com>
2021-06-27 02:12:10 +0300

    add custom timers with query params

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 window.addEventListener('load', function() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlParams.entries());
+  let b = document.createElement('b');
+  let title = document.createTextNode(params['title'] ?? "Sandorik's birthday");
+  b.appendChild(title);
+  document.getElementById('to').appendChild(b);
+  let target_date;
+  if (Object.entries(params).length == 0) {
+    target_date = {
+      month: 5,
+      day: 23,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
+  } else {
+    target_date = params;
+  }
+   
   let current_date = new Date();
+  let seconds = current_date.getUTCSeconds();
+  let minutes = current_date.getUTCMinutes();
+  let hours = current_date.getUTCHours();
   let day = current_date.getUTCDate();
   let month = current_date.getUTCMonth();
-  let year = (month > 5 || (month == 5 && day > 23)) ? current_date.getUTCFullYear() + 1 : current_date.getUTCFullYear();
-  console.log(year);
-  const COUNTDOWN_DATE = Date.UTC(year, 5, 23, 0, 0, 0);
+  let year = (month > target_date.month || (month == target_date.month && day > target_date.day) || (day == target_date.day && hours > target_date.hours) || (hours == target_date.hours && minutes > target_date.minutes) || (minutes == target_date.minutes && seconds > target_date.seconds)) ? current_date.getUTCFullYear() + 1 : current_date.getUTCFullYear();
+  
+  const COUNTDOWN_DATE = Date.UTC(year, target_date.month, target_date.day, target_date.hours ?? 0, target_date.minutes ?? 0, target_date.seconds ?? 0);
+  if (!COUNTDOWN_DATE) {
+    let e = document.createElement('p');
+    let text = document.createTextNode('In query parameters you should specify at least \"month\" and \"day\"');
+    e.appendChild(text);
+    b.appendChild(e);
+    return
+  }
 
   let digitsElements = {};
   ['days', 'hours', 'minutes', 'seconds'].forEach((id) => {
